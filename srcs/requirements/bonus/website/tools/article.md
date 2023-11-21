f1bd450c7d0614b3b9978b92c25b6599aee91c62
lletourn <lletourn@student.42.fr>
2023-11-21 12:06:15 +0100

    article + wordpress fix

--- srcs/requirements/bonus/website/tools/article.md
+++ srcs/requirements/bonus/website/tools/article.md
@@ -1,5 +1,6 @@
-Title: Sample Title
+Title: This is my static website
 Date: 2023-10-28 12:47
-Category: Test
+Category: Article
 
-This is a sample paragraph
+This page was created using Pelican, a static site generator written in Python.
+I wanted to make my own html/css static page but got lazy on the way (and busy with IRC) so here's my low effort bonus.
--- srcs/requirements/wordpress/tools/wordpress_config.sh
+++ srcs/requirements/wordpress/tools/wordpress_config.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sleep 5
+# sleep 5
 
     echo "\n==============================="
     echo "=== Wordpress configuration ==="
@@ -10,25 +10,36 @@ if [ -f "/var/www/html/wp-config.php" ]
 then
     echo "==> wordpress is already installed and configured\n"
 else
+    # download the wordpress core
     wp core download --allow-root
+
+    # configure the wordpress DB
     wp core config  --allow-root \
                     --dbname=$SQL_DATABASE \
                     --dbuser=$SQL_USER \
                     --dbpass=$SQL_PASSWORD \
                     --dbhost=$SQL_HOST
+
+    # install wordpress
     wp core install --allow-root\
                     --url=$DOMAIN\
                     --title=$WP_TITLE\
                     --admin_user=$WP_ADMIN\
                     --admin_password=$WP_ADMIN_PASSWORD\
                     --admin_email=$WP_ADMIN_EMAIL
-   wp user create $WP_USER $WP_USER_EMAIL\
+
+    # create a non-admin user
+    wp user create $WP_USER $WP_USER_EMAIL\
                     --role=author\
                     --user_pass=$WP_USER_PASSWORD\
                     --allow-root
 fi
 
+# ensure that the web server has the necessary permissions to read and write to those files
 chown -R www-data:www-data /var/www/html/wp-content
+chown -R www-data:www-data /var/www/html
+
+
 
     echo "\n==========================="
     echo "=== Redis configuration ==="
@@ -40,5 +51,7 @@ chown -R www-data:www-data /var/www/html/wp-content
     wp plugin install redis-cache --activate --allow-root
     wp plugin update --all --allow-root
     wp redis enable --allow-root
+
+chown -R www-data:www-data /var/www/html/wp-content/plugins/redis-cache
 
 exec /usr/sbin/php-fpm7.4 -F
